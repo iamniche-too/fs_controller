66e0825fdedc5fe058faf90bf6762dad796e13ce
iamniche-too <nicholas.oliver.hemley@gmail.com>
2020-02-28 14:27:28 +0000

    Added queues.

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # fs_controller
 
+Steps to set-up the controller:
+
+1) Install and run the queue daemon (Beanstalkd)
+
+```shell
+sudo apt-get install beanstalkd
+./beanstalkd -l 127.0.0.1 -p 12000
+```
+
 ```shell
 pipenv shell
 pipenv install --dev
--- controller.py
+++ controller.py
@@ -1,7 +1,11 @@
 import time
+import greenstalk
 
 class Controller:
     configurations = []
+
+    consumer_throughput_queue = greenstalk.Client(host='127.0.0.1', port=12000, watch='consumer_throughput')
+    producer_count_queue = greenstalk.Client(host='127.0.0.1', port=12000, watch='producer_count')
 
     def run(self):
         self.load_configurations()
@@ -33,6 +37,14 @@ class Controller:
             producer_increment_interval_sec = configuration["producer_increment_interval_sec"]
             print(f"Waiting {producer_increment_interval_sec} seconds.")
             time.sleep(producer_increment_interval_sec)
+            print("Reading producer_count queue...")
+            job = self.producer_count_queue.reserve()
+            if job:
+                producer_count = int(job.body)
+                print(f"Producer count={producer_count}")
+                # now remove from the queue
+                self.producer_count_queue.delete(job)
+
         print("Run completed.")
 
 
@@ -40,13 +52,15 @@ class Controller:
         print("Loading configurations.")
         # TODO - load from file?
         configuration_0 = {"number_of_brokers": 3, "message_size_kb": 750, "max_producers": 3, "producer_increment_interval_sec": 1}
-        #configuration_1 = {"number_of_brokers": 5, "message_size_kb": 750, "max_producers": 3,
-                           "producer_increment_interval_sec": 2}
-        #configuration_2 = {"number_of_brokers": 7, "message_size_kb": 750, "max_producers": 5,
-                           "producer_increment_interval_sec": 1}
+
+        # configuration_1 = {"number_of_brokers": 5, "message_size_kb": 750, "max_producers": 3,
+        #                   "producer_increment_interval_sec": 2}
+        # configuration_2 = {"number_of_brokers": 7, "message_size_kb": 750, "max_producers": 5,
+        #                  "producer_increment_interval_sec": 1}
+
         self.configurations.append(configuration_0)
-        #self.configurations.append(configuration_1)
-        #self.configurations.append(configuration_2)
+        # self.configurations.append(configuration_1)
+        # self.configurations.append(configuration_2)
 
 
 if __name__ == '__main__':
