226f140da49eb097b2c9c1dbb4bbba1aa8ac42fb
iamniche-too <nicholas.oliver.hemley@gmail.com>
2020-03-05 17:46:01 +0000

    Fixed namespace issue.

--- scripts/configure-producers.sh
+++ scripts/configure-producers.sh
@@ -5,7 +5,7 @@ echo "configure producers, MESSAGE_SIZE_KB=$MESSAGE_SIZE_KB"
 
 source ./export-gcp-credentials.sh
 
-kubectl -n producer-consumers apply -f - --kubeconfig ./kubeconfig.yaml<<EOF
+kubectl -n producer-consumer apply -f - --kubeconfig ./kubeconfig.yaml<<EOF
 ---
 apiVersion: apps/v1
 kind: Deployment
