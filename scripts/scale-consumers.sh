8bc65d4121d5e725c6e7d51358fc60f4bda9cc40
iamniche-too <nicholas.oliver.hemley@gmail.com>
2020-03-12 16:57:33 +0000

    scale-consumers.sh

--- scripts/scale-consumers.sh
+++ scripts/scale-consumers.sh
@@ -5,4 +5,4 @@ echo "configure consumers, NUM_CONSUMERS=$NUM_CONSUMERS"
 
 source ./export-gcp-credentials.sh
 
-kubectl -n producer-consumer scale consumers --replicas=$NUM_CONSUMERS --kubeconfig ./kubeconfig.yaml
+kubectl -n producer-consumer scale deployments consumer --replicas=$NUM_CONSUMERS --kubeconfig ./kubeconfig.yaml
