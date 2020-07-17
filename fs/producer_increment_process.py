7ad0ccecfe0b339e6d37fbc38defb74c2b06a167
iamniche-too <nicholas.oliver.hemley@gmail.com>
2020-07-17 13:27:00 +0100

    Wait for initial producers before starting increment.

--- fs/producer_increment_process.py
+++ fs/producer_increment_process.py
@@ -12,30 +12,31 @@ class ProducerIncrementProcess(BaseProcess):
         self.configuration = configuration
         self.consumer_throughput_process = consumer_throughput_process
 
-    def wait(self):
+    def wait_interval(self):
         # Wait for a specified interval before starting the next producer
         producer_increment_interval_sec = self.configuration["producer_increment_interval_sec"]
         print(f"[ProducerIncrementProcess] - Waiting {producer_increment_interval_sec}s before starting next producer.")
         time.sleep(producer_increment_interval_sec)
 
-    def run(self):
-        initial_producer_count = self.configuration["start_producer_count"]
-
+    def check_producer_count(self, desired_producer_count):
         actual_producer_count = self.get_producer_count()
         i = 1
-        while not self.is_stopped() and actual_producer_count < initial_producer_count:
+        while not self.is_stopped() and actual_producer_count < desired_producer_count:
             time.sleep(5)
             actual_producer_count = self.get_producer_count()
-
             if i % 5 == 0:
-                print(f"[ProducerIncrementProcess] - waiting for producer(s) to start, actual_producer_count={actual_producer_count}, initial_producer_count={initial_producer_count}")
-
+                print(f"[ProducerIncrementProcess] - actual_producer_count={actual_producer_count}, desired_producer_count={desired_producer_count}")
             i += 1
 
-        self.wait()
+        print(f"[ProducerIncrementProcess] - actual_producer_count={actual_producer_count}")
+
+    def run(self):
+        initial_producer_count = self.configuration["start_producer_count"]
+        self.check_producer_count(initial_producer_count)
+        self.wait_interval()
 
         # increment the producer count
-        desired_producer_count = actual_producer_count
+        desired_producer_count = initial_producer_count
         while not self.is_stopped() and (desired_producer_count < self.configuration["max_producer_count"]):
             desired_producer_count += 1
 
@@ -43,4 +44,5 @@ class ProducerIncrementProcess(BaseProcess):
                 print(f"[ProducerIncrementProcess] - Starting producer, desired_producer_count={desired_producer_count}")
                 self.k8s_scale_producers(desired_producer_count)
 
-            self.wait()
+            self.check_producer_count(initial_producer_count)
+            self.wait_interval()
