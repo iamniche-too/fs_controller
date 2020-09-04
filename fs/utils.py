fb5b825e4456cba51c6ecd31447d6c56e8b1576a
iamniche-too <nicholas.oliver.hemley@gmail.com>
2020-09-04 17:16:05 +0100

    typo

--- fs/utils.py
+++ fs/utils.py
@@ -22,7 +22,7 @@ DEFAULT_THROUGHPUT_MB_S = 75
 SEVENTY_FIVE_MBPS_IN_GBPS = 0.6
 
 # TODO - use a static Service Account rather than create one each time we provision the infra
-SERVICE_ACCOUNT_EMAIL = "cluster-minimal-368b7c901257@kafka-k8s-trial.iam.gserviceaccount.comm"
+SERVICE_ACCOUNT_EMAIL = "cluster-minimal-368b7c901257@kafka-k8s-trial.iam.gserviceaccount.com"
 
 CLUSTER_NAME = "gke-kafka-cluster"
 CLUSTER_ZONE = "europe-west2-a"
